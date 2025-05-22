7ee8a893ec9607826f37d56d4fbfbdf7e6abfbd0
Akbar Doosti <83670691+akbardoosti@users.noreply.github.com>
2025-05-22 20:38:58 +0330

    Fixed error in class-notification.php

--- includes/classes/class-notification.php
+++ includes/classes/class-notification.php
@@ -7,7 +7,15 @@ class RNOMPA_Notification {
         global $wpdb;
         $output = [];
         $table_name = $wpdb->prefix . 'telba_status';
-        $results = $wpdb->get_results("SELECT * FROM `$table_name` WHERE `is_checked` = '0'");
+
+        $cache_key = 'rnompa_telba_status_unchecked';
+        $results = wp_cache_get($cache_key, 'rnompa');
+
+        if ($results === false) {
+            $results = $wpdb->get_results("SELECT * FROM `$table_name` WHERE `is_checked` = '0'");
+            wp_cache_set($cache_key, $results, 'rnompa', 60); // cache for 60 seconds
+        }
+
         foreach ($results as $value) {
             if ('order' == $value->post_type) {
                 $output[] = [
@@ -34,6 +42,8 @@ class RNOMPA_Notification {
                 ];
             }
             $wpdb->update($table_name, ['is_checked' => 1], ['post_id' => $value->post_id]);
+            // Invalidate cache since the table has changed
+            wp_cache_delete($cache_key, 'rnompa');
         }
         return $output;
     }
